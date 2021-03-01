507d70ea73d00fbbd816057d6309bac5f6d30b8e
miyudai <miyudai4649@gmail.com>
2021-03-01 12:34:43 +0900

    スライドショーアプリ

---
+++ app/src/main/java/jp/techacademy/daichi/miyuki/autoslideshowapp/MainActivity.kt
@@ -0,0 +1,183 @@
+package jp.techacademy.daichi.miyuki.autoslideshowapp
+
+import android.Manifest
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.content.pm.PackageManager
+import android.os.Build
+import android.util.Log
+import android.provider.MediaStore
+import android.content.ContentUris
+import android.database.Cursor
+import android.os.Handler
+import android.view.View
+import android.widget.ImageView
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    private val PERMISSIONS_REQUEST_CODE = 100
+    private var cursor: Cursor? = null
+    private var mTimer: Timer? = null
+    private var mHandler = Handler()
+
+    private fun getContentsInfo() {
+        // 画像を取得
+        val resolver = contentResolver
+        val cursor = resolver.query(
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI, // データの種類
+            null, // 項目（null = 全項目）
+            null, // フィルタ条件（null = フィルタなし）
+            null, // フィルタ用パラメータ
+            null // ソート (nullソートなし）
+        )
+
+        if (cursor!!.moveToFirst()) {
+                // indexからIDを取得し、そのIDから画像のURIを取得する
+                val fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID)
+                val id = cursor.getLong(fieldIndex)
+                val imageUri =
+                    ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
+
+                imageView.setImageURI(imageUri)
+
+        }
+
+        //押したら画像が進むボタン
+        go_button.setOnClickListener {
+
+            if (cursor!!.moveToNext()) {
+                val fieldIndex = cursor!!.getColumnIndex(MediaStore.Images.Media._ID)
+                val id = cursor!!.getLong(fieldIndex)
+                val imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
+
+                imageView.setImageURI(imageUri)
+            }
+            else(cursor!!.moveToFirst()) {
+                val fieldIndex = cursor!!.getColumnIndex(MediaStore.Images.Media._ID)
+                val id = cursor!!.getLong(fieldIndex)
+                val imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
+
+                imageView.setImageURI(imageUri)
+            }
+        }
+
+        //押したら画像が戻るボタン
+        back_button.setOnClickListener {
+
+            if (cursor!!.moveToPrevious()) {
+                val fieldIndex = cursor!!.getColumnIndex(MediaStore.Images.Media._ID)
+                val id = cursor!!.getLong(fieldIndex)
+                val imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
+
+                imageView.setImageURI(imageUri)
+            }
+
+            else(cursor!!.moveToLast()) {
+                val fieldIndex = cursor!!.getColumnIndex(MediaStore.Images.Media._ID)
+                val id = cursor!!.getLong(fieldIndex)
+                val imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
+
+                imageView.setImageURI(imageUri)
+            }
+        }
+        //再生で2秒の自動送り、もっかい押すと停止する
+        start_button.setOnClickListener {
+
+            if (mTimer == null){
+                start_button.text = "停止"
+                go_button.isEnabled  = false
+                back_button.isEnabled  = false
+                mTimer = Timer()
+
+                mTimer!!.schedule(object : TimerTask() {
+                    override fun run() {
+                        mHandler.post {
+
+                                if(cursor!!.moveToNext()) {
+                                    val fieldIndex =
+                                        cursor!!.getColumnIndex(MediaStore.Images.Media._ID)
+                                    val id = cursor!!.getLong(fieldIndex)
+                                    val imageUri = ContentUris.withAppendedId(
+                                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                                        id
+                                    )
+
+                                    imageView.setImageURI(imageUri)
+                                }else(cursor!!.moveToFirst()) {
+                                    val fieldIndex = cursor!!.getColumnIndex(MediaStore.Images.Media._ID)
+                                    val id = cursor!!.getLong(fieldIndex)
+                                    val imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
+
+                                    imageView.setImageURI(imageUri)
+                                }
+                        }
+            }
+            }, 2000, 2000)
+        }else{
+                start_button.text = "再生"
+                go_button.isEnabled  = true
+                back_button.isEnabled  = true
+                if (mTimer != null){
+                    mTimer!!.cancel()
+                    mTimer = null
+                }
+            }
+            }
+
+
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            PERMISSIONS_REQUEST_CODE ->
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    getContentsInfo()
+                }
+
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        //ファイルの許可
+        // Android 6.0以降の場合
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            // パーミッションの許可状態を確認する
+            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                // 許可されている
+                getContentsInfo()
+            } else {
+                // 許可ダイアログを表示するようにする
+                requestPermissions(
+                    arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                    PERMISSIONS_REQUEST_CODE
+                )
+            }
+            // Android 5系以下の場合
+        } else {
+            getContentsInfo()
+        }
+
+
+
+
+
+
+
+    }
+
+
+}
+
+private operator fun <T> Comparable<T>.invoke(function: () -> Unit) {
+
+}
+
+
+
+
